12e78fc844e25f6ee4b90afd37b64df351fabf54
nvdai2401 <nvdai2401@gmail.com>
2020-02-28 17:02:17 +0700

    added loader, spliting code  and configured webpack for production env

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,49 @@
+/**
+ * TODO:
+ * - Optimize split chunk, vendor, common, minify js by using terser-webpack-plugin
+ * - Config ApolloGraphQL, Unit & Integration test
+ * - Setup project structure
+ *
+ */
+
 const { resolve } = require('path')
 const webpack = require('webpack')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
-module.exports = ({ mode }) => {
+module.exports = ({ mode, analyze }) => {
+	const isEnvDevelopment = mode === 'development'
+	const isEnvProduction = mode === 'production'
+	let minimizer = []
+	let plugins = [
+		new HtmlWebpackPlugin({
+			title: 'CleVer',
+			template: resolve('./src/index.html'),
+			inject: true,
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+			},
+		}),
+		new webpack.ProgressPlugin((percentage, message, ...args) => {
+			console.log(`${(percentage * 100).toFixed()}% ${message}`, ...args)
+		}),
+		new CleanWebpackPlugin(),
+	]
+
+	if (isEnvProduction) {
+		minimizer.push(new UglifyJsPlugin(), new OptimizeCSSAssetsPlugin())
+	}
+
+	if (analyze) {
+		plugins.push(new BundleAnalyzerPlugin())
+	}
+
 	return {
 		mode,
 		context: resolve(__dirname, 'src'),
@@ -13,7 +52,7 @@ module.exports = ({ mode }) => {
 		},
 		output: {
 			path: resolve(__dirname, 'dist'),
-			filename: '[name].[chunkhash].js',
+			filename: '[name].[chunkhash:8].js',
 			publicPath: '/',
 		},
 		resolve: {
@@ -34,6 +73,18 @@ module.exports = ({ mode }) => {
 						loader: 'babel-loader',
 					},
 				},
+				{
+					test: /\.(scss|css)$/i,
+					use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				},
+				{
+					test: /\.(png|jpe?g|gif|svg)$/i,
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'assets/images/',
+					},
+				},
 			],
 		},
 		devServer: {
@@ -42,20 +93,22 @@ module.exports = ({ mode }) => {
 			historyApiFallback: true,
 			inline: true,
 		},
-		plugins: [
-			new HtmlWebpackPlugin({
-				title: 'CleVer',
-				template: resolve('./src/index.html'),
-				inject: true,
-				minify: {
-					removeComments: true,
-					collapseWhitespace: true,
+		devtool: isEnvProduction ? 'source-map' : 'eval-cheap-source-map',
+		plugins,
+		optimization: {
+			minimize: isEnvProduction, //true by default in production mode
+			minimizer,
+			splitChunks: {
+				cacheGroups: {
+					default: false,
+					vendors: false,
+					commons: {
+						test: /[\\/]node_modules[\\/]/,
+						name: false,
+						chunks: 'all',
+					},
 				},
-			}),
-			new webpack.ProgressPlugin((percentage, message, ...args) => {
-				console.log(`${(percentage * 100).toFixed()}% ${message}`, ...args)
-			}),
-			new CleanWebpackPlugin(),
-		],
+			},
+		},
 	}
 }
