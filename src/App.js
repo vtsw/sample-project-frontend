9dc3b03233cfb4056ca7c08945523344259b4a1b
michael.nguyen <michael.nguyen@ewoosoft.com>
2020-03-11 08:02:53 +0700

    improved authen depend on token

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Suspense, lazy } from 'react'
 import { Route, Switch, withRouter } from 'react-router-dom'
 
 import { Box } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
+import { withStyles } from '@material-ui/core/styles'
+
+import { getToken } from '@src/shares/utils'
 
 import { NavBar } from '@views_components'
 
@@ -12,35 +14,55 @@ const User = lazy(() => import('@views/User'))
 const SignIn = lazy(() => import('@views/SignIn'))
 const SignUp = lazy(() => import('@views/SignUp'))
 
-const useStyles = makeStyles({
+const styles = {
 	root: {
 		display: 'flex',
 	},
-})
+}
 
-const App = ({ location }) => {
-	const classes = useStyles()
-	const shouldRenderNavBar = () => {
+class App extends React.Component {
+	componentDidMount() {
+		this.onRouteChanged()
+	}
+	componentDidUpdate(prevProps) {
+		if (this.props.location !== prevProps.location) {
+			this.onRouteChanged()
+		}
+	}
+
+	onRouteChanged() {
+		const authToken = getToken()
+		if (!authToken && this.props.location.pathname !== '/sign-in') {
+			this.props.history.push('/sign-in')
+			return
+		}
+	}
+	shouldRenderNavBar = () => {
+		const { location } = this.props
 		if (location.pathname === '/sign-in' || location.pathname === '/sign-up') {
 			return null
 		}
 		return <NavBar />
 	}
-	return (
-		<Box className={classes.root}>
-			{shouldRenderNavBar()}
 
-			<Suspense fallback={<div>Loading...</div>}>
-				<Switch>
-					<Route exact path='/' component={Main} />
-					<Route path='/message' component={Message} />
-					<Route path='/user' component={User} />
-					<Route path='/sign-in' component={SignIn} />
-					<Route path='/sign-up' component={SignUp} />
-				</Switch>
-			</Suspense>
-		</Box>
-	)
+	render() {
+		const { classes } = this.props
+		return (
+			<Box className={classes.root}>
+				{this.shouldRenderNavBar()}
+
+				<Suspense fallback={<div>Loading...</div>}>
+					<Switch>
+						<Route exact path='/' component={Main} />
+						<Route path='/message' component={Message} />
+						<Route path='/user' component={User} />
+						<Route path='/sign-in' component={SignIn} />
+						<Route path='/sign-up' component={SignUp} />
+					</Switch>
+				</Suspense>
+			</Box>
+		)
+	}
 }
 
-export default withRouter(App)
+export default withRouter(withStyles(styles)(App))
