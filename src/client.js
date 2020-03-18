67a8f833a38cc6a22eb7dca6b2a12e47f95578b3
michael.nguyen <michael.nguyen@ewoosoft.com>
2020-03-18 15:35:07 +0700

    refactored code

--- src/client.js
+++ src/client.js
@@ -2,7 +2,6 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
-import gql from 'graphql-tag'
 
 import { getToken } from './shares/utils'
 import localConfigs from './configs.local'
