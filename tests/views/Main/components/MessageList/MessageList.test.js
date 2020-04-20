0bae598fd817d5cdcdbf2835898e098d05c42823
michaelnguyen2401 <michael.nguyen@ewoosoft.com>
2020-04-20 15:14:44 +0700

    refactored MessageList's tests of Main

--- tests/views/Main/components/MessageList/MessageList.test.js
+++ tests/views/Main/components/MessageList/MessageList.test.js
@@ -1,158 +1,23 @@
-import React from 'react'
-import { findDOMNode } from 'react-dom'
+import { act, waitFor } from '@testing-library/react'
 
-import { act, fireEvent, waitFor } from '@testing-library/react'
-import { MockedProvider } from '@apollo/react-testing'
 import waait from 'waait'
 
-import { MessageList } from '@views/Main/components'
-
 import {
-	mockUserList,
-	mockMessageList,
-	renderDOMNode,
-	getMarkup,
-} from '@tests/shares/utils'
-import { FETCH_USER_LIST } from '@views/User/gql/query'
-import { MESSAGE_LIST } from '@views/Message/gql/query'
-import { DELETE_MESSAGE, UPDATE_MESSAGE } from '@views/Message/gql/mutation'
-import { PAGE_LIMIT } from '@src/configs.local'
+	mockMessage,
+	openModifyDialog,
+	openDeleteDialog,
+	modifyMessage,
+	findDOMNodeOfMessageList,
+} from './utils'
 
-const mocks = [
-	{
-		request: {
-			query: FETCH_USER_LIST,
-			variables: {
-				query: { limit: PAGE_LIMIT },
-			},
-		},
-		result: {
-			data: {
-				userList: {
-					items: mockUserList,
-					hasNext: true,
-				},
-			},
-		},
-	},
-	{
-		request: {
-			query: MESSAGE_LIST,
-			variables: {
-				query: { userId: mockUserList[0].id, limit: PAGE_LIMIT },
-			},
-		},
-		result: {
-			data: {
-				messageList: {
-					items: mockMessageList,
-					hasNext: true,
-					total: 30,
-				},
-			},
-		},
-	},
-	{
-		request: {
-			query: DELETE_MESSAGE,
-			variables: {
-				id: mockUserList[0].id,
-			},
-		},
-		result: {
-			data: {
-				deleteMessage: mockMessageList[0],
-			},
-		},
-	},
-]
-
-const resolvers = {
-	Query: {
-		userSearchValueOfMain: () => {
-			return ''
-		},
-		selectedUserOfMain: () => {
-			return mockUserList[0]
-		},
-	},
-	Mutation: {
-		setUserSearchValueOfMain: (_, { searchValue }, { cache }) => {
-			cache.writeData({
-				data: {
-					userSearchValueOfMain: searchValue,
-				},
-			})
-			return searchValue
-		},
-	},
-}
-
-const openModifyDialog = async rendered => {
-	const row = rendered.querySelectorAll(
-		`[data-testid=row-${mockMessageList[0].id}]`
-	)[0]
-
-	await act(async () => {
-		await row.click()
-	})
-
-	const modifyDialogTitle = document.querySelectorAll(
-		`[data-testid=modifydialog-title]`
-	)[0]
-
-	expect(modifyDialogTitle).toBeTruthy()
-}
-
-const openDeleteDialog = async rendered => {
-	const closeIcon = rendered.querySelectorAll(
-		`[data-testid=row-closeicon-${mockMessageList[0].id}]`
-	)[0]
-
-	fireEvent.click(closeIcon)
-
-	const deleteDialogTitle = document.querySelectorAll(
-		`[data-testid=deletedialog-title]`
-	)[0]
-
-	expect(deleteDialogTitle).toBeTruthy()
-}
-
-const modifyMessage = async message => {
-	const input = document.querySelectorAll(`[placeholder=placeholder]`)[0]
-	const agreeButton = document.querySelectorAll(
-		'[data-testid=modifydialog-agreebutton]'
-	)[0]
-
-	await act(async () => {
-		fireEvent.change(input, { target: { value: message } })
-	})
-
-	expect(input.value).toBe(message)
-
-	await act(async () => {
-		await agreeButton.click()
-	})
-}
-
-describe('MessageList', async () => {
+describe('MessageList', () => {
 	it('should match snapshot', async () => {
-		const rendered = findDOMNode(
-			renderDOMNode(
-				getMarkup(
-					<MockedProvider
-						mocks={mocks}
-						addTypename={false}
-						resolvers={resolvers}
-					>
-						<MessageList />
-					</MockedProvider>
-				)
-			)
-		)
-		await act(async () => {
-			await waait(10)
+		let rendered
+		act(async () => {
+			rendered = findDOMNodeOfMessageList()
 		})
+
+		await waait(10)
 
 		expect(rendered).toMatchSnapshot()
 	})
@@ -161,20 +26,9 @@ describe('MessageList', async () => {
 		let rendered
 
 		beforeEach(() => {
-			rendered = findDOMNode(
-				renderDOMNode(
-					getMarkup(
-						<MockedProvider
-							mocks={mocks}
-							addTypename={false}
-							resolvers={resolvers}
-						>
-							<MessageList />
-						</MockedProvider>
-					)
-				)
-			)
+			rendered = findDOMNodeOfMessageList()
 		})
+
 		it('should allow to click No button to close dialog', async () => {
 			await openModifyDialog(rendered)
 
@@ -196,7 +50,7 @@ describe('MessageList', async () => {
 		it('should allow to modify message', async () => {
 			await waait(10)
 			await openModifyDialog(rendered)
-			await modifyMessage('nvdai')
+			await modifyMessage(mockMessage)
 
 			await waitFor(() => {
 				expect(
@@ -225,7 +79,7 @@ describe('MessageList', async () => {
 			})
 		})
 
-		it('should allow to click MuiBackdrop-root to close dialog', async () => {
+		it('should allow to click MuiBackdrop-root to close ModifyDialog', async () => {
 			await waait(10)
 			await openModifyDialog(rendered)
 
@@ -247,24 +101,10 @@ describe('MessageList', async () => {
 		let rendered
 
 		beforeEach(() => {
-			rendered = findDOMNode(
-				renderDOMNode(
-					getMarkup(
-						<MockedProvider
-							mocks={mocks}
-							addTypename={false}
-							resolvers={resolvers}
-						>
-							<MessageList />
-						</MockedProvider>
-					)
-				)
-			)
+			rendered = findDOMNodeOfMessageList()
 		})
-		it('allow to click Yes button', async () => {
-			jest
-				.spyOn(window, 'alert')
-				.mockImplementation(() => 'Error: GraphQL error')
+
+		it('should allow to click Yes button', async () => {
 			await waait(10)
 			await openDeleteDialog(rendered)
 
@@ -276,8 +116,6 @@ describe('MessageList', async () => {
 				agreeButton.click()
 			})
 
-			expect(window.alert).toHaveBeenCalled()
-
 			await waitFor(() => {
 				expect(
 					document.querySelectorAll(`[data-testid=deletedialog-title]`)[0]
@@ -285,7 +123,7 @@ describe('MessageList', async () => {
 			})
 		})
 
-		it('allow to click No button', async () => {
+		it('should allow to click No button', async () => {
 			await waait(10)
 			await openDeleteDialog(rendered)
 
@@ -303,7 +141,8 @@ describe('MessageList', async () => {
 				).toBeFalsy()
 			})
 		})
-		it('allow to click MuiBackdrop-root to close dialog', async () => {
+
+		it('should allow to click MuiBackdrop-root to close DeleteDialog', async () => {
 			await waait(10)
 			await openDeleteDialog(rendered)
 
---
+++ tests/views/Main/components/MessageList/utils.js
@@ -0,0 +1,161 @@
+import React from 'react'
+import { findDOMNode } from 'react-dom'
+
+import { act, fireEvent } from '@testing-library/react'
+import { MockedProvider } from '@apollo/react-testing'
+
+import { MessageList } from '@views/Main/components'
+
+import {
+	mockUserList,
+	mockMessageList,
+	renderDOMNode,
+	getMarkup,
+} from '@tests/shares/utils'
+import { FETCH_USER_LIST } from '@views/User/gql/query'
+import { MESSAGE_LIST } from '@views/Message/gql/query'
+import { DELETE_MESSAGE, UPDATE_MESSAGE } from '@views/Message/gql/mutation'
+import { PAGE_LIMIT } from '@src/configs.local'
+
+const mockMessage = 'abc'
+
+const mocks = [
+	{
+		request: {
+			query: FETCH_USER_LIST,
+			variables: {
+				query: { limit: PAGE_LIMIT },
+			},
+		},
+		result: {
+			data: {
+				userList: {
+					items: mockUserList,
+					hasNext: true,
+				},
+			},
+		},
+	},
+	{
+		request: {
+			query: MESSAGE_LIST,
+			variables: {
+				query: { userId: mockUserList[0].id, limit: PAGE_LIMIT },
+			},
+		},
+		result: {
+			data: {
+				messageList: {
+					items: mockMessageList,
+					hasNext: true,
+					total: 30,
+				},
+			},
+		},
+	},
+	{
+		request: {
+			query: DELETE_MESSAGE,
+			variables: {
+				id: mockMessageList[0].id,
+			},
+		},
+		result: {
+			data: {
+				deleteMessage: mockMessageList[0],
+			},
+		},
+	},
+	{
+		request: {
+			query: UPDATE_MESSAGE,
+			variables: {
+				message: { id: mockMessageList[0].id, content: mockMessage },
+			},
+		},
+		result: {
+			data: {
+				updateMessage: mockMessageList[0],
+			},
+		},
+	},
+]
+
+const resolvers = {
+	Query: {
+		selectedUserOfMain: () => {
+			return mockUserList[0]
+		},
+	},
+}
+
+const openModifyDialog = async rendered => {
+	const row = rendered.querySelectorAll(
+		`[data-testid=row-${mockMessageList[0].id}]`
+	)[0]
+
+	await act(async () => {
+		fireEvent.click(row)
+	})
+
+	const modifyDialogTitle = document.querySelectorAll(
+		`[data-testid=modifydialog-title]`
+	)[0]
+
+	expect(modifyDialogTitle).toBeTruthy()
+}
+
+const openDeleteDialog = async rendered => {
+	const closeIcon = rendered.querySelectorAll(
+		`[data-testid=row-closeicon-${mockMessageList[0].id}]`
+	)[0]
+
+	await act(async () => {
+		fireEvent.click(closeIcon)
+	})
+
+	const deleteDialogTitle = document.querySelectorAll(
+		`[data-testid=deletedialog-title]`
+	)[0]
+
+	expect(deleteDialogTitle).toBeTruthy()
+}
+
+const modifyMessage = async message => {
+	const input = document.querySelectorAll(`[placeholder=placeholder]`)[0]
+	const agreeButton = document.querySelectorAll(
+		'[data-testid=modifydialog-agreebutton]'
+	)[0]
+
+	await act(async () => {
+		fireEvent.change(input, { target: { value: message } })
+	})
+
+	expect(input.value).toBe(message)
+
+	await act(async () => {
+		fireEvent.click(agreeButton)
+	})
+}
+
+const findDOMNodeOfMessageList = () => {
+	return findDOMNode(
+		renderDOMNode(
+			getMarkup(
+				<MockedProvider mocks={mocks} addTypename={false} resolvers={resolvers}>
+					<MessageList />
+				</MockedProvider>
+			)
+		)
+	)
+}
+
+export {
+	mockMessage,
+	mocks,
+	resolvers,
+	openModifyDialog,
+	openDeleteDialog,
+	modifyMessage,
+	findDOMNodeOfMessageList,
+}
